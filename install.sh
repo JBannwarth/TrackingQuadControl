5b77a2093fe8cf8ba2f28fa9016f37d8d61fc8f7
JBannwarth <jeremie.bannwarth@gmail.com>
2015-05-18 09:47:55 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,6 +59,9 @@ git clone https://github.com/mavlink/mavlink.git
 
 
 #installing dependancies
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get update
 sudo apt-get install ros-hydro-sensor-msgs python-serial python-tz
 sudo rosdep init
 rosdep update
